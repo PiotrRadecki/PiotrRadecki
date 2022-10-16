f8483bbd6982914c7b7242448c69a82e6a2a2010
piotrek <piotr.radecki00@gmail.com>
2022-10-16 19:38:01 +0200

    Zaj3.2

---
+++ PiotrRadecki/MainViewModels.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Xaml;
+
+namespace PiotrRadecki
+{
+    internal class MainViewModels : INotifyPropertyChanged
+    {
+        public MainViewModels()
+        {
+            ScreenVal = "0";
+            AddNumberCommand = new RelayCommand(AddNumber);
+            AddNumberCommand = new RelayCommand(AddOperation);
+        }
+
+        private void AddOperation(object obj)
+        {
+
+        }
+
+        private void AddNumber(object obj)
+        {
+            
+        }
+
+        public ICommand AddNumberCommand { get; set; }
+
+        private string _screenVal;
+        private string ScreenVal
+        {
+            get { return _screenVal; }
+            set
+            {
+                _screenVal = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+
+        public event PropertyChangedEventHandler
+            PropertyChanged;
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
--- PiotrRadecki/MainWindow.xaml.cs
+++ PiotrRadecki/MainWindow.xaml.cs
@@ -1,47 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace PiotrRadecki
 {
     public partial class MainWindow : Window
     {
-        enum Operation
-        {
-            none = 0,
-            add,
-            sub,
-            mult,
-            div,
-            result
-        }
-
-        private Operation m_eLastOperationSelected = Operation.none;
-        private void NumberButton_Click(object oSender, RoutedEventArgs eRoutedEventArgs)
-        {
-            if (Operation.result == m_eLastOperationSelected)
-            {
-                txtDisplay.Text = string.Empty;
-                m_eLastOperationSelected = Operation.none;
-            }
-            Button oButton = (Button)oSender;
-            txtDisplay.Text += oButton.Content;
-        }
-
+   
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = new MainViewModels();
         }
     }
 }
