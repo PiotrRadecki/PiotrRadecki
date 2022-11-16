463d2c0f1432fe99c409018e755d472b128df521
piotrek <piotr.radecki00@gmail.com>
2022-11-16 11:29:50 +0100

    Zaj3.5

---
+++ ProjektKolkoiKrzyzyk/Game.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows;
+
+namespace ProjektKolkoiKrzyzyk
+{
+    internal class Game
+    {
+        private ObservableCollection<string> _tab;
+
+        public ObservableCollection<string> tab
+        {
+            get { return _tab; }
+            set
+            {
+                _tab = value;
+
+                OnPropertyChanged("tab");
+                this.ValuateWinCndition();
+            }
+        }
+
+        // Event handler for INotifyPropertyChanged 
+        public event PropertyChangedEventHandler PropertyChanged;
+        void OnPropertyChanged(string propName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propName));
+
+            }
+        }
+
+        public Game()
+        {
+            tab = new ObservableCollection<string>();
+            for (int i = 0; i < 9; i++)
+                tab.Add("");
+        }
+        public void ValuateWinCndition()
+        {
+            if (tab.Count == 9)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    if (tab[i * 3] == tab[i * 3 + 1] && tab[i * 3 + 1] == tab[i * 3 + 2])
+                    {
+                        if (tab[i * 3] != "")
+                            this.EndGame(tab[i * 3]);
+                    }
+                    if (tab[i] == tab[i + 3] && tab[i + 3] == tab[i + 6])
+                    {
+                        if (tab[i] != "")
+                            this.EndGame(tab[i]);
+                    }
+
+                }
+                if ((tab[0] == tab[4] && tab[4] == tab[8]) || (tab[2] == tab[4] && tab[4] == tab[6]))
+                {
+                    if (tab[4] != "")
+                        this.EndGame(tab[4]);
+                }
+
+            }
+        }
+        private void EndGame(string a)
+        {
+            MessageBox.Show("Game won by " + a);
+            for (int i = 0; i < 9; i++)
+            {
+                tab[i] = "";
+            }
+
+        }
+    }
+}
+
--- ProjektKolkoiKrzyzyk/MainWindow.xaml.cs
+++ ProjektKolkoiKrzyzyk/MainWindow.xaml.cs
@@ -1,58 +1,60 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
 
 namespace ProjektKolkoiKrzyzyk
 {
 
     public partial class MainWindow : Window
     {
-        public bool IsPlayer1Turn { get; set; } = true;
-        public int Counter { get; set; }
+        Game gra = new Game();
+
+        string User = "X";
 
         public MainWindow()
         {
             InitializeComponent();
-            NewGame();
-        }
-
-        public void NewGame()
-        {
-            IsPlayer1Turn = false;
-            Counter = 0;
-
-            Button_0_0.Content = string.Empty;
-            Button_0_0.Content = string.Empty;
-            Button_0_0.Content = string.Empty;
-            Button_0_0.Content = string.Empty;
-            Button_0_0.Content = string.Empty;
-            Button_0_0.Content = string.Empty;
-            Button_0_0.Content = string.Empty;
-            Button_0_0.Content = string.Empty;
-            Button_0_0.Content = string.Empty;
-
+            ctlGrid.DataContext = gra;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            IsPlayer1Turn ^= true;
-            Counter++;
-
-            if (Counter > 9)
+            Button btn = (Button)sender;
+            gra.tab[Convert.ToInt16(btn.Tag)] = User;
+            if (User == "X")
+                User = "O";
+            else
+                User = "X";
+            gra.ValuateWinCndition();
+        }
+    }
+    class StringToBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value is string && (string)value == "")
             {
-                NewGame();
-                return;
+                return true;
+            }
+            else
+            {
+                return false;
             }
 
-            var button = sender as Button;
+        }
 
-            button.Content = "X";
-
-            if (IsPlayer1Turn)
-                button.Content = "O";
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value is Boolean && (Boolean)value)
+            {
+                return "";
+            }
             else
-                button.Content = "X";
+            {
+                return "";
+            }
 
-            button.Content = IsPlayer1Turn ? "O" : "X";
         }
     }
 }
