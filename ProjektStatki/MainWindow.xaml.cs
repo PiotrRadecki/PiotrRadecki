73cbb03af0cada4babfb6015f7042a3a36511b0b
piotrek <piotr.radecki00@gmail.com>
2023-01-07 14:47:43 +0100

    Gotowe Statki

--- ProjektStatki/MainWindow.xaml.cs
+++ ProjektStatki/MainWindow.xaml.cs
@@ -23,6 +23,92 @@ namespace ProjektStatki
         public MainWindow()
         {
             InitializeComponent();
+            int[] tab = new int[99];
+            int[] tab2 = new int[99];
+            Game gra = new Game(tab, tab2);
+            plnPersonForm.DataContext = gra;
+            Gracz2 okno = new Gracz2();
+            okno.DataContext= gra;
+            okno.Show();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button btn = (Button)sender;
+            if (((Game)plnPersonForm.DataContext).PersonIdOne[Convert.ToInt32(btn.Tag.ToString())] == 0)
+                ((Game)plnPersonForm.DataContext).PersonIdOne[Convert.ToInt32(btn.Tag.ToString())]++;
+            else if (((Game)plnPersonForm.DataContext).PersonIdOne[Convert.ToInt32(btn.Tag.ToString())] == 1)
+                ((Game)plnPersonForm.DataContext).PersonIdOne[Convert.ToInt32(btn.Tag.ToString())]--;
+        }
+
+        private void Button_Click_shot(object sender, RoutedEventArgs e)
+        {
+            Button btn = (Button)sender;
+            if (((Game)plnPersonForm.DataContext).PersonIdTwo[Convert.ToInt32(btn.Tag.ToString())] == 0 || ((Game)plnPersonForm.DataContext).PersonIdTwo[Convert.ToInt32(btn.Tag.ToString())] == 1)
+                ((Game)plnPersonForm.DataContext).PersonIdTwo[Convert.ToInt32(btn.Tag.ToString())] += 2;
         }
     }
+
+    public class YesNoToBooleanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            switch (value)
+            {
+                case 3:
+                    return new SolidColorBrush(Colors.Red);
+                case 2:
+                    return new SolidColorBrush(Colors.Yellow);
+                case 1:
+                    return new SolidColorBrush(Colors.Black);
+                case 0:
+                    return new SolidColorBrush(Colors.Transparent);
+            }
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value is Colors)
+            {
+                //if ((Colors)value == Colors.Red)
+                //    return 4;
+                //else
+                //    return 0;
+            }
+            return 0;
+        }
+    }
+
+    public class YesNoToBooleanConverter2 : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            switch (value)
+            {
+                case 3:
+                    return new SolidColorBrush(Colors.Red);
+                case 2:
+                    return new SolidColorBrush(Colors.Yellow);
+                case 1:
+                    return new SolidColorBrush(Colors.Transparent);
+                case 0:
+                    return new SolidColorBrush(Colors.Transparent);
+            }
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value is Colors)
+            {
+                //if ((Colors)value == Colors.Red)
+                //    return 4;
+                //else
+                //    return 0;
+            }
+            return 0;
+        }
+    }
+
 }
