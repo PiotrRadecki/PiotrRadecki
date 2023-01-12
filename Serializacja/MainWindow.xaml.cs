3bc1775c2e8e962f1c2be3ee5a29929238235cb7
piotrek <piotr.radecki00@gmail.com>
2023-01-12 16:09:12 +0100

    1.1

--- Serializacja/MainWindow.xaml.cs
+++ Serializacja/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,17 +13,61 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml;
+using System.Xml.Serialization;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Serializacja
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        List<Person> listOfPersons = new List<Person>();
         public MainWindow()
         {
-            InitializeComponent();
+        InitializeComponent();
+            if (File.Exists("test.xml"))
+            {
+                listOfPersons = Serializacjaa.DeserializeToObject<List<Person>>("\"\\\\Mac\\Home\\Desktop\"");            }
+            else
+            {
+                listOfPersons.Add(new Person("Imie", "Nazwisko"));
+                listOfPersons.Add(new Person("Imie", "Nazwisko"));
+                listOfPersons.Add(new Person("Imie", "Nazwisko"));
+                listOfPersons.Add(new Person("Imie", "Nazwisko"));
+                dataGridPerson.ItemsSource = listOfPersons;
+            }
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 okno = new Window1();
+            Person osoba = new Person();
+            okno.DataContext = osoba;
+            okno.ShowDialog();
+            if (okno.IsOkPressed)
+            {
+                listOfPersons.Add(osoba);
+                dataGridPerson.Items.Refresh();
+            }
+        }
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (dataGridPerson.SelectedItem != null)
+            {
+                Window1 okno = new Window1();
+                Person osoba = new Person((Person)dataGridPerson.SelectedItem);
+                okno.DataContext = osoba;
+                okno.ShowDialog();
+                if (okno.IsOkPressed)
+                {
+                    int index = listOfPersons.IndexOf((Person)dataGridPerson.SelectedItem);
+                    listOfPersons[index] = osoba;
+                    dataGridPerson.Items.Refresh();
+                }
+            }
+        }
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            Serializacjaa.SerializeToXml<List<Person>>(listOfPersons, @"C:\\Users\\piotrek\\Source\\Repos\\PiotrRadecki\\Serializacja\readAndSave\test.xml");
         }
     }
 }
---
+++ Serializacja/Person.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Serializacja
+{
+    [XmlRoot(ElementName = "Osoby")]
+    public class Person
+    {
+        [XmlAttribute("FirstName")]
+        public string Imie { get; set; }
+        [XmlAttribute("LastName")]
+        public string Nazwisko { get; set; }
+
+        public Person()
+        {
+
+        }
+        public Person(string Imie, string Nazwisko)
+        {
+            this.Imie = Imie;
+            this.Nazwisko = Nazwisko;
+        }
+
+        public Person(Person osoba)
+        {
+            this.Imie = osoba.Imie;
+            this.Nazwisko = osoba.Nazwisko;
+        }
+    }
+}
