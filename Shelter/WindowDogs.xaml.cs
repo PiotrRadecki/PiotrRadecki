9d614b00b823ec80803b5a334fcc76c85a22abb6
piotrek <piotr.radecki00@gmail.com>
2023-01-24 01:59:32 +0100

    1.3

--- Shelter/WindowDogs.xaml.cs
+++ Shelter/WindowDogs.xaml.cs
@@ -10,36 +10,33 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Shelter
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Logika interakcji dla klasy WindowDogs.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class WindowDogs : Window
     {
-        public MainWindow()
+        public WindowDogs()
         {
             InitializeComponent();
         }
-
         private void Button_Click_Close(object sender, RoutedEventArgs e)
         {
             this.Close();
         }
-
         private void Button_Click_Cats(object sender, RoutedEventArgs e)
         {
             WindowCats wincats = new WindowCats();
             wincats.Show();
             this.Close();
         }
-        private void Button_Click_Dogs(object sender, RoutedEventArgs e)
+        private void Button_Click_Home(object sender, RoutedEventArgs e)
         {
-            WindowDogs windogs = new WindowDogs();
-            windogs.Show();
+            MainWindow winhome = new MainWindow();
+            winhome.Show();
             this.Close();
         }
     }
